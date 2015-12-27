30e2596b0b981d35f6f56b0840e2348a3d2fb1f3
WoodHome <694547528@qq.com>
2015-12-27 22:24:49 +0800

    wificache

--- logger/src/main/java/com/sb/logger/NetTransport/WifiCacheUploader.java
+++ logger/src/main/java/com/sb/logger/NetTransport/WifiCacheUploader.java
@@ -1,23 +1,55 @@
 package com.sb.logger.NetTransport;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
 
 import com.sb.logger.Transports.LogRecord;
+
+import java.util.ArrayList;
 
 /**
  * Created by WoodHome on 2015/12/27.
  */
-public class WifiCacheUploader extends AbstractNetLogUploader {
+public class WifiCacheUploader extends AbstractNetLogUploader implements Runnable {
+
+    private static final int time_interval = 60 * 1000;
+    private static final int cache_count = 10;
 
     Context mContext ;
+    Handler mHandler;
+    ArrayList<LogRecord> logRecords = new ArrayList<>();
 
     public WifiCacheUploader(Context context){
         super(new DefaultNetUploader());
         mContext = context;
+        mHandler = new Handler();
+        mHandler.postDelayed(this,time_interval);
     }
 
     @Override
     public void upload(LogRecord record) {
+        logRecords.add(record);
+    }
 
+    private static boolean isWifi(Context mContext) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) mContext
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        if (activeNetInfo != null
+                && activeNetInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void run() {
+        if(logRecords.size() > cache_count && isWifi(mContext)){
+            getNetUploader().upload(logRecords);
+            logRecords.clear();
+        }
+        mHandler.postDelayed(this,time_interval);
     }
 }
